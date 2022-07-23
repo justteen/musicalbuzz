731572c55a0fd678f2e607ebe68d7e905aa41c0b
BUZZ <48372964+justteen@users.noreply.github.com>
2022-07-23 09:41:57 +0700

    Create __init__.py

---
+++ BuzzMusic/utils/__init__.py
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2021-2022 by Buzz@Github, < https://github.com/justteen >.
+#
+# This file is part of < https://github.com/justteen/musicalbuzz > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/justteen/musicalbuzz/blob/main/LICENSE >
+#
+# All rights reserved.
+
+from .channelplay import *
+from .database import *
+from .decorators import *
+from .formatters import *
+from .inline import *
+from .pastebin import *
+from .sys import *
